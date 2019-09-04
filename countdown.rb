e87d97431dc181eae110b8a523225edc4ed690ff
Roshanak Agahi <rosh.agahi@gmail.com>
2019-09-04 21:15:21 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,3 +9,13 @@ end
 puts "HAPPY NEW YEAR!"
 "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(count)
+  while count > 0
+  puts "#{count} SECONDS!"
+  sleep 1 
+  count -=1 
+end
+puts "HAPPY NEW YEAR!"
+"HAPPY NEW YEAR!"
+end
